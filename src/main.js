7ef531d6025745324e77a5833a97f5cb6bbf2d4d
Kylin <1159469891@qq.com>
2025-06-10 19:08:12 +0800

    refactor: 删除多余文件

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import '@/assets/css/cover.less';
 import '@/assets/css/common.less';
 
 import router from './router';
