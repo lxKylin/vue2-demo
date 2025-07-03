6be60be85affb392df1c7563c473374b8f3daa1d
Kylin <1159469891@qq.com>
2025-07-03 15:56:18 +0800

    fix: 修复无法运行

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -56,12 +56,7 @@ export default defineConfig({
     compress: true,
     host: 'localhost',
     open: true, //配置自动启动浏览器
-    proxy: {
-      // history模式下的url会请求到服务器端，但是服务器端并没有这一个资源文件，就会返回404，所以需要配置这一项
-      historyApiFallback: {
-        index: '/index.html' //与output的publicPath
-      }
-    }
+    proxy: {}
   },
   tools: {
     /**
